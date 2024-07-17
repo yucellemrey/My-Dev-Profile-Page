ef571b932b47a05b528c191decc08074eb8e6d53
yucellemrey <emreyucel97@outlook.com>
2024-07-17 17:30:43 +0300

    vercel v9

--- src/store/actions/profileActions.js
+++ src/store/actions/profileActions.js
@@ -1,36 +1,30 @@
 import axios from "axios";
 import data from "../../mockData/db.json";
 
-export const setProfile = (profile) => ({
-  type: "SET_PROFILE",
-  payload: profile,
-});
-
-export const setProfileLoading = (isLoading) => ({
-  type: "SET_PROFILE_LOADING",
-  payload: isLoading,
-});
+export const setProfile = (profile) => {
+  return {
+    type: "SET_PROFILE",
+    payload: profile,
+  };
+};
 
 export const fetchProfile = () => {
   return async (dispatch) => {
-    dispatch(setProfileLoading(true));
-
     const lang = localStorage.getItem("language");
-    console.log(lang);
 
     try {
+      console.log("Posting data to API...");
       const postResponse = await axios.post(
         "https://reqres.in/api/workintech",
         data[lang]
       );
 
+      console.log("Post response data:", postResponse.data);
       const result = postResponse.data;
 
       dispatch(setProfile(result.profile));
     } catch (error) {
       console.error("There was an error fetching the profile data!", error);
-    } finally {
-      dispatch(setProfileLoading(false));
     }
   };
 };
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -11,13 +11,15 @@ export const setProjects = (projects) => {
 export const fetchProjects = () => {
   return async (dispatch) => {
     const lang = localStorage.getItem("language");
-    console.log(lang);
+
     try {
+      console.log("Posting data to API...");
       const postResponse = await axios.post(
         "https://reqres.in/api/workintech",
         data[lang]
       );
 
+      console.log("Post response data:", postResponse.data);
       const result = postResponse.data;
 
       dispatch(setProjects(result.projects));
